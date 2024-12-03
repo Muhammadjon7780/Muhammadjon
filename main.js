9b6e1778d4ecd465713e984c985348aff2d1b7f8
Muhammadjon <muhammadjonismoiljono0@gmail.com>
2024-12-04 01:02:17 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+// if (true) {
+//   var blockVar = "This is block scope";
+//   // console.log(blockVar); // This is block scope
+// }
+
+// var blockVar = "defrgt"
+
+// console.log(blockVar);
+
+
+function myFunc() {
+  var funcVar = "This is function scope";
+  console.log(funcVar); // This is function scope
+}
+myFunc()
+// console.log(funcVar); // Xato: funcVar mavjud emas
